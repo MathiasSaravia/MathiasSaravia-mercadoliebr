bb2d0eb58c2f75f5f88df5ce14e5fbd3d26aebfe
MathiasSaravia <maycolbeach@gmail.com>
2024-03-05 20:17:06 -0300

    error de crear producto

---
+++ src/controllers/admin/index.js
@@ -0,0 +1,7 @@
+module.exports = {
+create:require("./create.controller"),
+edit:require("./edit.controller"),
+update:require("./updates.controller"),
+store:require("./store.controller"),
+destroy:require("./destroy.controller")  
+}
